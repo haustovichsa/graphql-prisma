3ea49bb161d5355797f89aea6dbf648a40c96846
Sergey_Khaustovich <haustovichsa@gmail.com>
2020-01-15 23:24:13 +0300

    separate the GraphQL server functionality

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import { GraphQLServer, PubSub } from 'graphql-yoga'
+import db from './db'
+import { resolvers, fragmentReplacements } from './resolvers/index'
+import prisma from './prisma'
+
+// Scalar types: String, Boolean, Int, Float, ID,
+
+const pubsub = new PubSub()
+
+const server = new GraphQLServer({
+    typeDefs: "./src/schema.graphql",
+    resolvers,
+    context(request) {
+       return {
+            db,
+            pubsub,
+            prisma,
+            request
+       }
+    },
+    fragmentReplacements,
+})
+
+export { server as default   }
